9ca510cb92e67c465594d4f0afc2e850c2aad3dc
jmc0320 <joshua.carlson@intel.com>
2023-01-04 23:03:42 +0000

    file copy test

--- test_ssh_module.py
+++ test_ssh_module.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest import mock
+import os
 
 from framework import ssh_connection
 from framework import utils
@@ -82,7 +83,16 @@ class ConnectionTests(unittest.TestCase):
         before = self.session.get_session_before(2)
 
     def test_copy_file_to(self):
-        pass
+        src = '/tmp/test_file.txt'
+        prompt = '#'
+        os.system(f"echo 'this is some text' > {src}")
+        self.session.copy_file_to(src)
+        output = self.session.send_expect('ls -la', prompt)
+        success = 'test_file.txt' in output
+        self.assertTrue(success)
+        self.session.send_expect('rm test_file.txt', prompt)
+        os.system(f'rm {src}')
+
 
     def test_copy_file_from(self):
         pass
